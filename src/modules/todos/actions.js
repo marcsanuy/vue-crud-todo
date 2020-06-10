8ee1b337526a89be1ed527d53ab32e0969220a89
marcsanuy <marcsanuy@gmail.com>
2020-06-10 10:55:34 +0200

    Actions to consume the API and pass to the Store

--- src/modules/todos/actions.js
+++ src/modules/todos/actions.js
@@ -9,6 +9,78 @@ export async function fetchTodos ({commit}) {
     } catch (e) {
         commit('todosError', e.message)
     } finally {
-        console.log('La petición para obtener lo todos ha finalizado')
+        console.log('La petición para obtener los todos ha finalizado')
+    }
+}
+
+export async function addTodo ({commit}, todo) {
+    try {
+        await Vue.axios({
+            method: 'POST',
+            url: '/todos',
+            data: {
+                id: Date.now(),
+                text: todo.text,
+                done: false
+            }
+        })
+        
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('La petición para crear un todo ha finalizado')
+    }
+}
+
+export async function updateTodo ({commit}, todo) {
+    try {
+        await Vue.axios({
+            method: 'PUT',
+            url: `/todos/${todo.id}`,
+            data: {
+                id: todo.id,
+                text: todo.text,
+                done: todo.done
+            }
+        })
+        
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('La petición para actualizar un todo ha finalizado')
+    }
+}
+
+export async function updateTodoStatus ({commit}, todo) {
+    try {
+        await Vue.axios({
+            method: 'PUT',
+            url: `/todos/${todo.id}`,
+            data: {
+                id: todo.id,
+                text: todo.text,
+                done: ! todo.done
+            }
+        })
+        
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('La petición para actualizar el estado de un todo ha finalizado')
+    }
+}
+
+export async function remove ({commit, dispatc}, id) {
+    try {
+        await Vue.axios({
+            method: 'DELETE',
+            url: `/todos/${todo.id}`,
+           
+        })
+        dispatch('fetchTodos')
+    } catch (e) {
+        commit('todosError', e.message)
+    } finally {
+        console.log('La petición para actualizar el estado de un todo ha finalizado')
     }
 }
