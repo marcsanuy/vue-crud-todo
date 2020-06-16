49ec162bfa607df57403913f83741194d01d613b
marcsanuy <marcsanuy@gmail.com>
2020-06-16 11:05:40 +0200

    Finish TodoItem component, write the template and use in TodoList

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,5 +1,31 @@
 <template>
-    
+    <b-row class="mb-2">
+        <b-col cols="2">{{ todo.text }}</b-col>
+        <b-col cols="1">{{ todo.done }}</b-col>
+        <b-col>
+            <b-button
+                variant="primary"
+                @click="goToUpdateTodo"
+            >
+                Editar
+            </b-button> 
+            <b-button
+                variant="warning"
+                class="ml-2"
+                @click="updateTodoStatus"
+            > 
+                Estado
+            </b-button> 
+            <b-button
+                variant="danger"
+                class="ml-2"
+                @click="removeTodo"
+            > 
+                Eliminar
+            </b-button> 
+
+        </b-col>
+    </b-row>
 </template>
 
 <script>
@@ -17,17 +43,17 @@ export default {
             }),
             ...mapMutations('todos', ['setTodo']),
             goToUpdateTodo (todo) {
-                this.setTodo(todo)
+                this.setTodo(this.todo)
                 this.$router.push({
                     name: 'todos-update',
-                    params: {id: todo.id}
+                    params: {id: this.todo.id}
                 })
             },
-            updateTodoStatus (todo) {
-                this._updateTodoStatus(todo)
+            updateTodoStatus () {
+                this._updateTodoStatus(this.todo)
             },
-            removeTodo (id) {
-                this._removeTodo(id)
+            removeTodo () {
+                this._removeTodo(this.todo.id)
             }   
         }
     }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,17 @@
 <template>
     <div v-if="todos.length">
+        <todo-item
+            v-for="todo in todos"
+            :key="todo.id"
+            :todo="todo"
+        ></todo-item>
 
     </div>
     <b-alert show variant="info" v-else>No hay Todos<b-alert>
 </template>
 
 <script>
+import TodoItem from './TodoItem'
 export default {
     props: {
         todos: {
