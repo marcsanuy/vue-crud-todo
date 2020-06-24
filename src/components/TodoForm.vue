39b368015a7f0121fdba10610f465bf0a2c027ba
marcsanuy <marcsanuy@gmail.com>
2020-06-24 11:03:21 +0200

    Develop logic of TodoForm component

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { validationMixin } from 'vuelidate'
+import { required , minLength } from 'vuelidate/lib/validators'
+export default {
+    mixins: [validationMixin],
+    props: {
+        todo: {
+            type: Object,
+            required: true
+        },
+        todoSubmit: {
+            type: String,
+            default: 'Crear Todo'
+        }
+    },
+    validations: {
+        todo: {
+            text: {
+                required, minLength: minLength(4)
+            }
+        }
+    }
+}
+</script>>
