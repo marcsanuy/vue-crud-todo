323a2927f6894deae060a4c57fa8f7d811843c50
marcsanuy <marcsanuy@gmail.com>
2020-07-02 11:12:38 +0200

    Update todos with todoForm component & finish

--- src/modules/todos/mutations.js
+++ src/modules/todos/mutations.js
@@ -1,20 +1,20 @@
 export function setTodos (state, todos) {
     state.todos = todos;
-}
-
-export function setTodo (state, todo) {
-    state.selectTodo = todo
-}
-
-export function updateTodoStatus (state, payload) {
-    const todo = state.todos.find(t => t.id ===payload.id)
+  }
+  
+  export function setTodo (state, todo) {
+    state.selectedTodo = todo
+  }
+  
+  export function updateTodoStatus (state, payload) {
+    const todo = state.todos.find(t => t.id === payload.id)
     if (todo) {
-        todo.done = ! todo.done
+      todo.done = ! todo.done
     }
-}
-
-export function todosError (state, payload) {
-  state.error = true
-  state.errorMessage = payload
-  state.todos = []
-}
+  }
+  
+  export function todosError (state, payload) {
+    state.error = true
+    state.errorMessage = payload
+    state.todos = []
+  }
